1ec4addb9febab6ce86c0d877e697d9204b39562
SimaoNunes <simaspedro1@gmail.com>
2018-11-28 18:16:36 +0000

    locais done

--- Exemplo_Lab/accounts.php
+++ Exemplo_Lab/accounts.php
@@ -1,5 +1,6 @@
 <html>
     <body>
+    <h3>Accounts</h3>
 <?php
     try
     {
@@ -7,26 +8,24 @@
         $user ="istxxxxx";
         $password = "xxxxxxx";
         $dbname = $user;
-        
-        
+    
         $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
         $sql = "SELECT account_number, branch_name, balance FROM account;";
+    
         $result = $db->prepare($sql);
         $result->execute();
-           
-        echo("<table border=\"1\">\n");
-        echo("<tr><td>account_number</td><td>branch_name</td><td>balance</td></tr>\n");
+    
+        echo("<table border=\"0\" cellspacing=\"5\">\n");
         foreach($result as $row)
         {
-            echo("<tr><td>");
-            echo($row['account_number']);
-            echo("</td><td>");
-            echo($row['branch_name']);
-            echo("</td><td>");
-            echo($row['balance']);
-            echo("</td></tr>\n");
+            echo("<tr>\n");
+            echo("<td>{$row['account_number']}</td>\n");
+            echo("<td>{$row['branch_name']}</td>\n");
+            echo("<td>{$row['balance']}</td>\n");
+            echo("<td><a href=\"balance.php?account_number={$row['account_number']}\">Change balance</a></td>\n");
+            echo("</tr>\n");
         }
         echo("</table>\n");
     
@@ -39,3 +38,4 @@
 ?>
     </body>
 </html>
+        
