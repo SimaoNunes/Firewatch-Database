ed42721b7f8a6245c5179a9d7ea5976842d378e3
SimaoNunes <simaspedro1@gmail.com>
2018-11-28 03:40:53 +0000

    webpages

---
+++ Projeto/database/1/Eventos/index.php
@@ -0,0 +1,116 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="icon" type="image/png" href= "http://www.pngall.com/wp-content/uploads/2016/04/Database-PNG.png">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <title>Eventos</title>
+        <style>
+            body{
+                background-color: #474747;
+            }
+            h3{
+                color: white;
+                text-align: center;
+            }
+            h6{
+                color: white;
+                text-align: center;
+                margin-left: 130;
+                margin-right: 130;
+            }
+            img{
+                cursor: pointer;
+            }
+            a{
+                margin: 1;
+            }
+            table{
+                color: white;
+                margin: 0 auto;
+            }
+            .centered{
+                margin: 20 auto;
+            }
+        </style>
+    </head>
+    <body>
+        
+        <div class="centered">
+            <h3>Inserir Evento de Emergência</h3>
+            <form action='index.php' method='post'>
+                <h6>Telefone: <input type='text' name='tlfn'/></h6>
+                <h6>Instante de Chamada: <input type='text' name='instante'/></h6>
+                <h6>Nome da Pessoa: <input type='text' name='nome'/></h6>
+                <h6>Morada: <input type='text' name='morada'/></h6>
+                <h6>Nº processo: <input type='text' name='n'/></h6>
+                <h6><input class="btn btn-success" type="submit" value="Submit"></h6>
+            </form>
+        </div>
+
+        <div class="container">
+            <table class="table col-md-12">
+                <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col">Instante</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Morada</th>
+                    <th scope="col">Nº Processo</th>
+                    <th scope="col">Remover</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+
+                $host = "db.ist.utl.pt";
+                $user ="ist186512";
+                $password = "fico6299";
+                $dbname = $user;
+            
+                $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+                $sql = "SELECT numtelefone, instantechamada, nomepessoa, moradalocal, numprocessosocorro FROM eventoemergencia;";
+                $result = $db->prepare($sql);
+                $result->execute();
+
+                $count = 0;
+                foreach($result as $row)
+                {
+                    echo("<tr>");
+                    echo("<td>");
+                    echo($count);
+                    echo("</td>");
+                    echo("<td>");
+                    echo($row['numtelefone']);
+                    echo("</td>");
+                    echo("<td>");
+                    echo($row['instantechamada']);
+                    echo("</td>");
+                    echo("<td>");
+                    echo($row['nomepessoa']);
+                    echo("</td>");
+                    echo("<td>");
+                    echo($row['moradalocal']);
+                    echo("</td>");
+                    echo("<td>");
+                    echo($row['numprocessosocorro']);
+                    echo("</td>");
+                    echo("<td>");
+                    echo("<img width='20' src='https://goo.gl/uJnJJD'>");
+                    echo("</td>");
+                    echo("<tr>");
+                    $count = $count + 1;
+                }
+        
+                $db = null;
+                ?>
+                </tbody>
+                
+            </table>
+        </div>
+            
+    </body>
+</html>
---
+++ Projeto/database/1/Meios/index.php
@@ -0,0 +1,106 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="icon" type="image/png" href= "http://www.pngall.com/wp-content/uploads/2016/04/Database-PNG.png">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <title>Meios</title>
+        <style>
+            body{
+                background-color: #474747;
+            }
+            h3{
+                color: white;
+                text-align: center;
+            }
+            h6{
+                color: white;
+                text-align: center;
+                margin-left: 130;
+                margin-right: 130;
+            }
+            img{
+                cursor: pointer;
+            }
+            a{
+                margin: 1;
+            }
+            table{
+                color: white;
+                margin: 0 auto;
+            }
+            .centered{
+                margin: 20 auto;
+            }
+        </style>
+    </head>
+    <body>
+        
+        <div class="centered">
+            <h3>Inserir Meio</h3>
+            <form action='index.php' method='post'>
+                <h6>Nº: <input type='text' name='n'/></h6>
+                <h6>Nome: <input type='text' name='nome'/></h6>
+                <h6>Entidade: <input type='text' name='entidade'/></h6>
+                <h6><input class="btn btn-success" type="submit" value="Submit"></h6>
+            </form>
+        </div>
+
+        <div class="container">
+            <table class="table col-md-8">
+                <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nº</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Entidade</th>
+                    <th scope="col">Remover</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+
+                $host = "db.ist.utl.pt";
+                $user ="ist186512";
+                $password = "fico6299";
+                $dbname = $user;
+            
+                $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+                $sql = "SELECT nummeio, nomemeio, nomeentidade FROM meio;";
+                $result = $db->prepare($sql);
+                $result->execute();
+
+                $count = 0;
+                foreach($result as $row)
+                {
+                    echo("<tr>");
+                    echo("<td>");
+                    echo($count);
+                    echo("</td>");
+                    echo("<td>");
+                    echo($row['nummeio']);
+                    echo("</td>");
+                    echo("<td>");
+                    echo($row['nomemeio']);
+                    echo("</td>");
+                    echo("<td>");
+                    echo($row['nomeentidade']);
+                    echo("</td>");
+                    echo("<td>");
+                    echo("<img width='20' src='https://goo.gl/uJnJJD'>");
+                    echo("</td>");
+                    echo("<tr>");
+                    $count = $count + 1;
+                }
+        
+                $db = null;
+                ?>
+                </tbody>
+                
+            </table>
+        </div>
+            
+    </body>
+</html>
