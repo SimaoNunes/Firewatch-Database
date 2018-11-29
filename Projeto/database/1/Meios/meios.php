f340cd207851e6bdad44aec1ebef5942c46b9973
SimaoNunes <simaspedro1@gmail.com>
2018-11-29 00:17:30 +0000

    meios done

--- Projeto/database/1/Meios/meios.php
+++ Projeto/database/1/Meios/meios.php
@@ -38,19 +38,85 @@
         
         <div class="centered">
             <h3>Inserir Meio</h3>
-            <form action='index.php' method='post'>
-                <h6>Nº: <input type='text' name='n'/></h6>
-                <h6>Nome: <input type='text' name='nome'/></h6>
-                <h6>Entidade: <input type='text' name='entidade'/></h6>
+            <form action='meios.php' method='post'>
+                <h6>Nº: <input type='number' name='n' require/></h6>
+                <h6>Nome: <input type='text' name='nome' require/></h6>
+                <h6>Entidade: <input type='text' name='entidade' require/></h6>
                 <h6><input class="btn btn-success" type="submit" value="Submit"></h6>
             </form>
         </div>
+
+        <?php 
+
+        if(isset($_REQUEST['n']) and isset($_REQUEST['nome']) and isset($_REQUEST['entidade'])){
+           
+            $num  = $_REQUEST['n']; 
+            $name = $_REQUEST['nome'];
+            $ent  = $_REQUEST['entidade'];
+
+            $host = "db.ist.utl.pt";
+            $user ="ist186512";
+            $password = "fico6299";
+            $dbname = $user;
+        
+            $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+            $sql = "INSERT INTO meio (nummeio,nomemeio,nomeentidade) VALUES (:num,:nome,:entidade);";
+        
+            $result = $db->prepare($sql);
+            $result->bindParam(':num', $num);
+            $result->bindParam(':nome', $name);
+            $result->bindParam(':entidade', $ent);
+            $result->execute();
+        
+            $db = null;
+
+            header("Refresh:0");
+        }
+        
+
+        if(isset($_REQUEST['remN']) and isset($_REQUEST['remNome']) and isset($_REQUEST['remEnt'])){
+          
+            $apagarN    = $_REQUEST['remN'];
+            $apagarNome = $_REQUEST['remNome'];
+            $apagarEnt  = $_REQUEST['remEnt'];    
+
+            echo($apagarN);
+            echo($apagarNome);
+            echo($apagarEnt);
+
+            $host = "db.ist.utl.pt";
+            $user ="ist186512";
+            $password = "fico6299";
+            $dbname = $user;
+        
+            $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+            $sql = "DELETE FROM local WHERE ( nummeio = (:apagarN) AND nomeentidade = (:apagarNome) AND  nomeentidade = (:apagarEntidade);";
+        
+            $result->bindParam(':apagarN', $apagarN);
+            $result->bindParam(':apagarNome', $apagarNome);
+            $result->bindParam(':apagarEntidade', $apagarEnt);
+
+            $result = $db->prepare($sql);
+            $result->execute();
+        
+            $db = null;
+
+            $newURL = 'http://web.tecnico.ulisboa.pt/~ist186512/projects/database/1/Meios/Meios.php';
+            header('Location: '.$newURL);
+
+        }
+
+        ?>
+
 
         <div class="container">
             <table class="table col-md-8">
                 <thead class="thead-dark">
                 <tr>
-                    <th scope="col">#</th>
                     <th scope="col">Nº</th>
                     <th scope="col">Nome</th>
                     <th scope="col">Entidade</th>
@@ -68,17 +134,13 @@
                 $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
                 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
-                $sql = "SELECT nummeio, nomemeio, nomeentidade FROM meio;";
+                $sql = "SELECT nummeio, nomemeio, nomeentidade FROM meio ORDER BY nomeentidade, nummeio ASC;";
                 $result = $db->prepare($sql);
                 $result->execute();
 
-                $count = 0;
                 foreach($result as $row)
                 {
                     echo("<tr>");
-                    echo("<td>");
-                    echo($count);
-                    echo("</td>");
                     echo("<td>");
                     echo($row['nummeio']);
                     echo("</td>");
@@ -89,10 +151,9 @@
                     echo($row['nomeentidade']);
                     echo("</td>");
                     echo("<td>");
-                    echo("<img width='20' src='https://goo.gl/uJnJJD'>");
+                    echo("<a href='meios.php?remN={$row['nummeio']}?remNome={$row['nomemeio']}?remEnt={$row['nomeentidade']}'><img width='20' src='https://goo.gl/uJnJJD'></a>");
                     echo("</td>");
                     echo("<tr>");
-                    $count = $count + 1;
                 }
         
                 $db = null;
